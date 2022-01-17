b74038b19ccd574de70c0d432939c74c29ad6067
Pu2Pa2 <fu2sak1@gmail.com>
2022-01-17 15:54:31 +0900

    Add JS 704.Binary search solution

---
+++ js/704.binary-search.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// const nums = [-1, 0, 3, 5, 9, 12];
+// const target = 9;
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = function (nums, target) {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        // console.log("left :", left, "right :", right);
+        const pivot = parseInt((left + right) / 2);
+        // console.log("pivot :", pivot, "nums[pivot] :", nums[pivot]);
+        if (target === nums[pivot]) {
+            return pivot;
+        } else {
+            if (target < nums[pivot]) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
+        }
+
+    }
+
+    return -1;
+};
+
+// console.log("result :", search(nums, target));
