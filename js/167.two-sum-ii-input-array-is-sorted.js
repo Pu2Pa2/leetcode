e8e6d065ee74eb8f4e88f70eff760e22f4f5e158
Pu2Pa2 <fu2sak1@gmail.com>
2022-01-20 14:43:28 +0900

    Add JS 167.Two sum ii input array is sorted solution

---
+++ js/167.two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=167 lang=javascript
+ *
+ * [167] Two Sum II - Input Array Is Sorted
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+// eslint-disable-next-line no-unused-vars
+var twoSum = function(numbers, target) {
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) {
+                return [i+1, j+1];
+            }
+        }
+    }
+};
+// @lc code=end
